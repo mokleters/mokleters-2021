40fbb0b95aaecf970bd0c723f7ea560f290a8cb8
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2021-12-13 22:42:10 +0700

    Organization day-1
    need coursel and more cards also ogranization not responsive

--- src/components/Organization.js
+++ src/components/Organization.js
@@ -1,10 +1,71 @@
 import React from "react";
+import '../css/Organization.css';
+import img1 from '../assets/organization/osis.png';
+import img2 from '../assets/organization/mpk.png';
+import img3 from '../assets/organization/pustel.png'
 
 export default class Organization extends React.Component {
+  
   render() {
-    return (
-      <section id="organization">
-        <h1 className="b-example-divider mb-0">Organization section</h1>
+    return (      
+      <section id="organization">        
+        <div className="orga text-center pb-4">
+          <h5>Check Out Our</h5>
+          <div className="varsity-text text-red mb-5">ORGANIZATIONS</div>               
+        </div>
+        <div class="containerr">
+          <div class="cardd">
+            <div class="atas"></div>
+            <div class="cardd-img">
+              <img src={img1} alt="/"/>
+            </div>
+            <div class="cardd-body">
+            <h3>
+                OSIS
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.                 
+              </p>
+              <span>learn more</span>
+            </div>
+          </div>
+          <div class="cardd">
+            <div class="atas"></div>
+            <div class="cardd-img">
+              <img src={img2} alt="/"/>
+            </div>
+            <div class="cardd-body">
+              <h3>
+                MPK
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.                 
+              </p>
+              <span>learn more</span>
+            </div>
+          </div>
+          <div class="cardd">
+            <div class="atas"></div>
+            <div class="cardd-img">
+              <img src={img3} alt="/"/>
+            </div>
+            <div class="cardd-body">
+            <h3>
+                PUSTEL
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.                 
+              </p>
+              <span>learn more</span>
+            </div>
+          </div>
+
+          <div class="backgroundd"></div>
+
+        </div>
       </section>
     );
   }
