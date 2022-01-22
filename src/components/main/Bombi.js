4effd1ce52937dc3ceeec064e00bbdd7d6beb0d6
Gagas Surya Laksana <gagassurya19@gmail.com>
2022-01-22 17:15:58 +0700

    fix responsive#bombi

--- src/components/main/Bombi.js
+++ src/components/main/Bombi.js
@@ -1,12 +1,11 @@
 import React from "react";
 import tridi from "../../assets/3d/Bombi_ReMetal.glb"
-import tridibg from "../../assets/3d/spruit_sunrise_4k.hdr"
 import bombi_image from "../../assets/3d/bombi.png"
 
 import "../../css/bombi.scoped.css";
 
 export default class Hero extends React.Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       isPlay: false,
@@ -24,27 +23,27 @@ export default class Hero extends React.Component {
         <section id="bombi">
           <div className="full-page">
             <div className="row g-0 vh-100">
-              <div className={this.state.isPlay ? this.state.isPlayOn_1:this.state.isPlayOff_1}>
+              <div className={this.state.isPlay ? this.state.isPlayOn_1 : this.state.isPlayOff_1}>
                 {this.state.isPlay === true ? (
-                <model-viewer 
-                bounds="tight" 
-                src={tridi}
-                ar ar-modes="webxr scene-viewer quick-look" 
-                camera-controls environment-image="neutral" 
-                poster={bombi_image}
-                shadow-intensity="1" 
-                camera-target="0m 6.85m 0.3887m" 
-                camera-orbit="-39.16deg 75deg auto" 
-                auto-rotate
-                style={{"background-color": "unset", width: "100%", height: "100%"}}/>
-                ):(
-                    <model-viewer 
-                      poster={bombi_image}
-                      style={{"background-color": "unset", width: "100%", height: "100%"}}/>
+                  <model-viewer
+                    bounds="tight"
+                    src={tridi}
+                    ar ar-modes="webxr scene-viewer quick-look"
+                    camera-controls environment-image="neutral"
+                    poster={bombi_image}
+                    shadow-intensity="1"
+                    camera-target="0m 6.85m 0.3887m"
+                    camera-orbit="-39.16deg 75deg auto"
+                    auto-rotate
+                    style={{ "background-color": "unset", width: "100%", height: "100%" }} />
+                ) : (
+                  <model-viewer
+                    poster={bombi_image}
+                    style={{ "background-color": "unset", width: "100%", height: "100%" }} />
                 )}
-                
+
               </div>
-              <div className={this.state.isPlay ? this.state.isPlayOn_2:this.state.isPlayOff_2}>
+              <div className={this.state.isPlay ? this.state.isPlayOn_2 : this.state.isPlayOff_2}>
                 <div class="d-flex align-items-center vh-100">
                   <div class="col col-lg-8 m-4">
                     <div class="text h-300 align-items-left justify-content-left text-left">
@@ -53,16 +52,24 @@ export default class Hero extends React.Component {
                         BOMBI
                       </h1>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                       </p>
-                      {this.state.isPlay === false ? (
-                        <a type="button" class="btn btn-danger shake"
-                        onClick={() => this.setState({isPlay: true})}>Play Now</a>
-                      ):(
-                        <a type="button" class="btn btn-danger"
-                          onClick={() => this.setState({isPlay: false})}>Stop Now</a>
-                      )}
-                      <a href="#blog" class="btn btn-danger m-3">Learn More</a>
+
+                      <div class="btn-toolbar">
+                        <div class="btn-group">
+                          {this.state.isPlay === false ? (
+                            <>
+                              <button type="button" class="d-none d-md-block btn btn-danger m-3" onClick={() => this.setState({ isPlay: true })}>Play Now</button>
+                              <button type="button" class="d-md-none btn btn-danger m-3" data-bs-toggle="modal" data-bs-target="#modal">Play on Mobile</button>
+                            </>
+                          ) : (
+                            <button type="button" class="btn btn-danger m-3" onClick={() => this.setState({ isPlay: false })}>Stop Now</button>
+                          )}
+                        </div>
+                        <div class="btn-group">
+                          <button href="#blog" class="btn btn-danger m-3">Learn More</button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -70,6 +77,31 @@ export default class Hero extends React.Component {
             </div>
           </div>
         </section>
+
+        <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body vh-100">
+                <div className={this.state.isPlayOn_1}>
+                <model-viewer
+                  bounds="tight"
+                  src={tridi}
+                  ar ar-modes="webxr scene-viewer quick-look"
+                  camera-controls environment-image="neutral"
+                  poster={bombi_image}
+                  shadow-intensity="1"
+                  camera-target="0m 6.85m 0.3887m"
+                  camera-orbit="-39.16deg 75deg auto"
+                  auto-rotate
+                  style={{ "background-color": "unset", width: "100%", height: "100%" }} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
