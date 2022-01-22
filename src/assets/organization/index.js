97c92862eb0717007be201f99abd7a5d9a2decfb
Gagas Surya Laksana <gagassurya19@gmail.com>
2022-01-23 00:49:19 +0700

    fix simplify#organization

---
+++ src/assets/organization/index.js
@@ -0,0 +1,37 @@
+import osis from './osis.jpg';
+import mpk from './mpk.png';
+import pustel from './pustel.png'
+import metic from './metic.jpeg';
+import bdi from './bdi.png';
+import comet from './comet.png';
+import da from './da.png';
+import mac from './mac.png';
+import memo from './memo.png';
+import palwaga from './palwaga.png';
+import paskatema from './paskatema.png';
+import pmi from './pmi.png';
+import tsbc from './tsbc.png';
+import tscc from './tscc.png';
+import tsfc from './tsfc.png';
+import tsvc from './tsvc.jpg';
+
+const dataImages = {
+    osis,
+    mpk,
+    pustel,
+    metic,
+    bdi,
+    comet,
+    da,
+    mac,
+    memo,
+    palwaga,
+    paskatema,
+    pmi,
+    tsbc,
+    tscc,
+    tsfc,
+    tsvc
+}
+
+export default dataImages;
